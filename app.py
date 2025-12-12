6ce04ed2acf9bcdfba215e4312b710d21a487fd8
s-yaseen-s <s-yaseen.bilal@zewailcity.edu.eg>
2025-12-13 00:51:08 +0200

    FIXES

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    # Run on local network - accessible from other devices
+    app.run(host='0.0.0.0', port=5000, debug=True)
